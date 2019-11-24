c2a70c7d39b980a95b2e934238e3e71aaee67f66
amanprince28 <amanprince28@gmail.com>
2019-11-24 20:45:44 +0530

    -

--- controller/tax.js
+++ controller/tax.js
@@ -19,13 +19,13 @@ const self = module.exports = {
             if(annualSalary > 0 && annualSalary <= 18200){
                 resolve( 'Nil' );
             }else if(annualSalary >= 18201 && annualSalary <= 37000){
-                resolve( Math.round((3572 + 0.325 * (annualSalary - 18200))/12) );
+                resolve( Math.round((0 + (0.19 * (annualSalary - 18200)))/12) );
             }else if(annualSalary >= 37001 && annualSalary <= 87000){
-                resolve( Math.round((3572 + 0.325 * (annualSalary - 37000))/12) );
+                resolve( Math.round((3572 + (0.325 * (annualSalary - 37000)))/12) );
             }else if(annualSalary >= 87001 && annualSalary <= 180000){
-                resolve( Math.round((3572 + 0.325 * (annualSalary - 87000))/12) );
+                resolve( Math.round((19822 + (0.37 * (annualSalary - 87000)))/12) );
             }else if(annualSalary >= 180001){
-                resolve( Math.round((3572 + 0.325 * (annualSalary - 180000))/12) );
+                resolve( Math.round((54232 + (0.45 * (annualSalary - 180000)))/12) );
             }
             
         })
